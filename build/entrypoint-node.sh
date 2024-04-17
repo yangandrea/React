9586aa7a1393c7f22f70d340e458bf214fe24c30
yangandrea <yang.andrea@itismeucci.com>
2024-04-17 21:14:56 +0200

    m

--- build/entrypoint-node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /app
-if [ ! -f package.json ]; then
-  rm .gitkeep
-  npx -y create-react-app . 
-fi
-npm install
-npm start
--- build/entrypoint-php.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-composer update
-/usr/sbin/apache2ctl -DFOREGROUND
